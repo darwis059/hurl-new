a7a8226e88892c602cd9fe8d378fe789c1b5588f
Darwis <darwis.e03@gmail.com>
2026-04-30 13:59:08 +0800

    only build

--- bin/release/release.ps1
+++ bin/release/release.ps1
@@ -29,22 +29,22 @@ New-Item -ItemType Directory -Force -Path $package_dir
 # Copy-Item -Path $lib_dir\iconv-2.dll -Destination $package_dir
 Copy-Item -Path $release_dir\hurl.exe -Destination $package_dir
 Copy-Item -Path $release_dir\hurlfmt.exe -Destination $package_dir
-((& $package_dir\hurl --version) -Split " ")[1] > $package_dir\version.txt
-Get-Content $package_dir\version.txt
+# ((& $package_dir\hurl --version) -Split " ")[1] > $package_dir\version.txt
+# Get-Content $package_dir\version.txt
 
 # add hurl to PATH
-$registry_user_path=(Get-ItemProperty -Path 'HKCU:\Environment').Path
-$registry_machine_path=(Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment').Path
-$env:Path = $actual_path + ";" + $package_dir + ";" + $registry_user_path + ";" + $registry_machine_path
-sleep 10
+# $registry_user_path=(Get-ItemProperty -Path 'HKCU:\Environment').Path
+# $registry_machine_path=(Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment').Path
+# $env:Path = $actual_path + ";" + $package_dir + ";" + $registry_user_path + ";" + $registry_machine_path
+# sleep 10
 
-# hurl infos
-(Get-Command hurl).Path
-(Get-Command hurlfmt).Path
-hurl --version
-if ($LASTEXITCODE) { Throw }
-hurlfmt --version
-if ($LASTEXITCODE) { Throw }
+# # hurl infos
+# (Get-Command hurl).Path
+# (Get-Command hurlfmt).Path
+# hurl --version
+# if ($LASTEXITCODE) { Throw }
+# hurlfmt --version
+# if ($LASTEXITCODE) { Throw }
 
 cd $actual_dir
 
